f034a3246585c26c4ea73b1ff4d3cfe4a559c32c
Psilon <pzixel@gmail.com>
2019-01-20 16:16:16 +0300

    first

--- src/main.rs
+++ src/main.rs
@@ -24,16 +24,17 @@ impl Matcher {
         match request.request_type {
             RequestType::FillOrKill => match request.side {
                 Side::Ask => {
-                    let has_sum = self
-                        .bids
-                        .iter()
-                        .filter(|x| x.request.price <= request.price)
-                        .map(|x| x.request.size)
-                        .scan(0, |s, x| {
-                            *s += x;
-                            Some(*s)
-                        })
-                        .any(|x| x >= request.price);
+                    let has_sum = self.has_sum(request);
+                    if !has_sum {
+                        return MatchingResult::Cancelled;
+                    }
+                    unimplemented!()
+                }
+                Side::Bid => unimplemented!(),
+            },
+            RequestType::Limit => match request.side {
+                Side::Ask => {
+                    let has_sum = self.has_sum(request);
                     if !has_sum {
                         return MatchingResult::Cancelled;
                     }
@@ -44,12 +45,38 @@ impl Matcher {
             _ => unimplemented!(),
         }
     }
+
+    fn has_sum(&self, request: Request) -> bool {
+        match request.side {
+            Side::Ask => self
+                .bids
+                .iter()
+                .filter(|x| x.request.price <= request.price)
+                .map(|x| x.request.size)
+                .scan(0, |s, x| {
+                    *s += x;
+                    Some(*s)
+                })
+                .any(|x| x >= request.price),
+            Side::Bid => self
+                .asks
+                .iter()
+                .filter(|x| x.request.price >= request.price)
+                .map(|x| x.request.size)
+                .scan(0, |s, x| {
+                    *s += x;
+                    Some(*s)
+                })
+                .any(|x| x <= request.price),
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::dto::*;
     use crate::Matcher;
+    use assert_matches::assert_matches;
 
     #[test]
     pub fn test_fill_or_kill_buy_empty() {
@@ -65,5 +92,31 @@ mod tests {
         let result = matcher.try_match(request);
 
         assert_eq!(result, MatchingResult::Cancelled);
+    }
+
+    #[test]
+    pub fn test_fill_or_kill_buy_success() {
+        let mut matcher = Matcher::new();
+        let bid_request = Request {
+            side: Side::Bid,
+            price: 10,
+            size: 10,
+            user_id: 0,
+            request_type: RequestType::Limit,
+        };
+
+        let ask_request = Request {
+            side: Side::Ask,
+            price: 10,
+            size: 10,
+            user_id: 0,
+            request_type: RequestType::FillOrKill,
+        };
+
+        let bid_result = matcher.try_match(bid_request);
+        let ask_result = matcher.try_match(ask_request);
+
+        assert_eq!(bid_result, MatchingResult::Queued);
+        assert_matches!(ask_result, MatchingResult::Executed(_));
     }
 }
